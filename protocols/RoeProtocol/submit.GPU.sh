99bdbaa865240613e292304d6f458cdb16e0f691
gitoliver <olgrant@uga.edu>
2024-08-29 16:22:50 +0200

    Needed a submit file and less sim time by default

---
+++ protocols/RoeProtocol/submit.GPU.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -J JOBNAME
+#SBATCH --partition=gm
+#SBATCH --get-user-env
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --gres=gpu:1
+
+source /etc/profile.d/modules.sh
+source /cm/shared/apps/amber20//amber.sh
+
+bash Run_Multi-Part_Simulation.bash
