054cb59e050612ff10573e5bd03880ac1ef11359
B. Lachele Foley <lachele@gmail.com>
2024-10-21 02:41:49 -0400

    Updating Prep-and-Min to use the new Run script

--- protocols/Glycan/Prep_and_Minimization/Run-Gas-Min-Parameters.bash
+++ protocols/Glycan/Prep_and_Minimization/Run-Gas-Min-Parameters.bash
@@ -8,23 +8,41 @@
 ##
 ## Here, all parameters are explicitly set, even those left at defaults.
 ##
+## The defaults correspond to simulations run via GLYCAM-Web.
+##
+## Values in this file can be overridden in a file called 
+##        Local_Run_Parameters.bash
+## This file is included at the bottom of this file, if it is found.
+##
 ################################################################################
 
 thisAMBERHOME='DETECT'
 PRMTOP='unminimized-gas.parm7'
 INPCRD='unminimized-gas.rst7'
+initialCoordFormat='Amber7Rst'   ## Amber 7 restart
 coordOutputFormat="NetCDF"  ## ntwo=2 - much smaller files; not human readable
-restrtSuffix='restrt.nc'
-mdSuffix='nc'
-outputFileName='gas_phase_minimization.log'
 
 mdEngine=sander
 useMpi=N
-numProcs=1
-useCuda=N
-allowOverwrites=N
+numProcs=4
+useCuda='N'
+allowOverwrites='N'
 
-writeCommands=Yes
+writeCommands='Yes'
 
-runPrefix[0]='min-gas'
-runDescription[0]='Gas-phase minimization'
+RunParts=( min )
+
+declare -A Prefix
+Prefix=(
+	[min]='min-gas'
+	)
+declare -A Description
+Description=(
+	[min]='Minimize gas-phase structure, dielectric=80'
+	)
+
+
+if [ -f "Local_Run_Parameters.bash" ] ; then
+	echo "found local info file"
+	. Local_Run_Parameters.bash
+fi
