57c44beacdcd43daeb6eb885b3da584c68c1fbbc
Grayson Miller <grayson.miller124@gmail.com>
2024-11-18 20:46:14 -0500

    Added a submit.CPU script for convenience

--- protocols/RoeProtocol/submit.CPU.sh
+++ protocols/RoeProtocol/submit.CPU.sh
@@ -5,9 +5,9 @@
 #SBATCH --output=slurmSimulation.out
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
-#SBATCH --gres=gpu:1
 
 source /etc/profile.d/modules.sh
 source "${AMBERHOME}/amber.sh"
 
 bash Run_Multi-Part_Simulation.bash
+    
